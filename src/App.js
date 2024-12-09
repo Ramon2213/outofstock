3783fcf0bc41b97131e4b40113f0489416ad2b50
Ramon2213 <ramondenboer1995@hotmail.com>
2024-12-09 14:58:33 +0100

    12

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import GroceryCart from "./components/grocerycart/GroceryCart";
 
-function App() {
+
+
+
+
+
+
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <GroceryCart />
     </div>
   );
 }
 
-export default App;
+
+
+
+
